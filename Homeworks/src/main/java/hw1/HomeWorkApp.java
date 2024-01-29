eef043b1dea68fdb6a3507129cfd52a4a76c80c4
Nijat <hamidov9jat@gmail.com>
2024-01-29 19:00:29 +0400

    added methods for task 4 and 5 along with comments to them. Also made changes to the task enumeration in comments

--- Homeworks/src/main/java/hw1/HomeWorkApp.java
+++ Homeworks/src/main/java/hw1/HomeWorkApp.java
@@ -8,7 +8,7 @@ public class HomeWorkApp {
     }
 
 
-    // Task 1 Метод печатает 3 слова в столбец
+    // Task 2 Метод печатает 3 слова в столбец
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
@@ -16,7 +16,7 @@ public class HomeWorkApp {
 
     }
 
-    // Task 2 Метод на проверку знака суммы
+    // Task 3 Метод на проверку знака суммы
     public static void checkSumSign() {
         int a = 23, b = 54;
 
@@ -29,10 +29,29 @@ public class HomeWorkApp {
         }
     }
 
+    // Task 4 Метод выводит на экран цвета сфетофора
+    // в зависимости от значения переменной value
     public static void printColor() {
         int value = 37;
 
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
 
+    // Task 5 Метод для сравнения двух локальныйх переменных
+    public static void compareNumbers() {
+        int a = 23, b = 89;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
     }
 
 }
