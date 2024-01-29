0cae340409fb15668a406e8665b61f59a8443e85
Nijat <hamidov9jat@gmail.com>
2024-01-29 18:45:43 +0400

    Renamed project in pom file to homeworks

--- Homeworks/src/main/java/hw1/HomeWorkApp.java
+++ Homeworks/src/main/java/hw1/HomeWorkApp.java
@@ -2,7 +2,9 @@ package hw1;
 
 public class HomeWorkApp {
     public static void main(String[] args) {
-        printThreeWords();
+//        printThreeWords();
+
+
     }
 
 
@@ -12,6 +14,17 @@ public class HomeWorkApp {
         System.out.println("Banana");
         System.out.println("Apple");
 
+    }
+
+    // Task 2
+    public static void checkSumSign() {
+        int a = 23, b = 54;
+
+        if (a + b > 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 
 }
