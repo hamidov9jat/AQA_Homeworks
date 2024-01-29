bafbe320044f79dfdcd9ad9e64ee24b96f491ce3
Nijat <hamidov9jat@gmail.com>
2024-01-29 18:47:53 +0400

    added comment for Task 2

--- Homeworks/src/main/java/hw1/HomeWorkApp.java
+++ Homeworks/src/main/java/hw1/HomeWorkApp.java
@@ -16,11 +16,13 @@ public class HomeWorkApp {
 
     }
 
-    // Task 2
+    // Task 2 Метод на проверку знака суммы
     public static void checkSumSign() {
         int a = 23, b = 54;
 
-        if (a + b > 0) {
+        int sum = a + b;
+
+        if (sum > 0) {
             System.out.println("Сумма положительная");
         } else {
             System.out.println("Сумма отрицательная");
