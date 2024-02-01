95179cde7a23849e5138c00e3eb1e37cee8330dc
Nijat <hamidov9jat@gmail.com>
2024-02-01 21:34:49 +0400

    added task 2 for lesson 2

--- src/main/java/homework2/DriverProgram.java
+++ src/main/java/homework2/DriverProgram.java
@@ -3,9 +3,13 @@ package homework2;
 public class DriverProgram {
     public static void main(String[] args) {
 
-        // Testing method from task one
+        // Testing method from task 1
         int x1 = 23, x2 = -7;
         System.out.println(TaskOne.within10and20(x1, x2)); // true
         System.out.println(TaskOne.within10and20(x1, x2-20)); //false
+
+        // Testing method from task 2
+        TaskTwo.printIsPositiveOrNegative(x1); // positive
+        TaskTwo.printIsPositiveOrNegative(x2); // negative
     }
 }
---
+++ src/main/java/homework2/TaskTwo.java
@@ -0,0 +1,9 @@
+package homework2;
+
+public class TaskTwo {
+
+    // Task 2 Method which prints whether the number is non-negative
+    public static void printIsPositiveOrNegative(int x) {
+        System.out.println((x >= 0 ? "Число положтельное" : "Число отрицательное"));
+    }
+}
