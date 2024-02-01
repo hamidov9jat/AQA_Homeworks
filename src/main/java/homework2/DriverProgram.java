46a0f7bb483846335eb699462d819fecc0bb38d2
Nijat <hamidov9jat@gmail.com>
2024-02-01 22:44:46 +0400

    added and tested task 9*

--- src/main/java/homework2/DriverProgram.java
+++ src/main/java/homework2/DriverProgram.java
@@ -1,12 +1,14 @@
 package homework2;
 
+import java.util.Arrays;
+
 public class DriverProgram {
     public static void main(String[] args) {
 
         // Testing method from task 2
         int x1 = 23, x2 = -7;
         System.out.println(TaskTwo.within10and20(x1, x2)); // true
-        System.out.println(TaskTwo.within10and20(x1, x2-20)); //false
+        System.out.println(TaskTwo.within10and20(x1, x2 - 20)); //false
 
         // Testing method from task 3
         TaskThree.printIsPositiveOrNegative(x1); // positive
@@ -31,6 +33,10 @@ public class DriverProgram {
         // Testing task 8
         TaskEight.fillDiagonal();
 
-
+        // Testing task 9
+        assert TaskNine.initialValueArray(5, 78) != null;
+        System.out.println(Arrays.toString(TaskNine.initialValueArray(5, 78)));
+        assert TaskNine.initialValueArray(4, 34) != null;
+        System.out.println(Arrays.toString(TaskNine.initialValueArray(4, 34)));
     }
 }
