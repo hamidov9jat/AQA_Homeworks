3b6664cb692ed3cde862a597b7198862435266b2
Nijat <hamidov9jat@gmail.com>
2024-02-05 19:01:24 +0400

    created DriverProgram and Employee class

---
+++ src/main/java/homework3/DriverProgram.java
@@ -0,0 +1,22 @@
+package homework3;
+
+/*
+Task 4. Создать массив из 5 сотрудников.
+Пример:
+// Вначале объявляем массив объектов
+Person[] persArray = new Person[5];
+
+// потом для каждой ячейки массива задаем объект
+persArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+persArray[1] = new Person(...);
+...
+persArray[4] = new Person(...);
+
+5. С помощью цикла вывести информацию только о сотрудниках старше 40 лет.
+ */
+
+public class DriverProgram {
+    public static void main(String[] args) {
+
+    }
+}
