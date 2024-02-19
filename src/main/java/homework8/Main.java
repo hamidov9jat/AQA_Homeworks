d87b3fd12949d693441a9381cd7fb5dd58e7e010
Nijat <hamidov9jat@gmail.com>
2024-02-19 19:27:37 +0400

    Task 2 completed and tested (WordStats)

--- src/main/java/homework8/Main.java
+++ src/main/java/homework8/Main.java
@@ -8,6 +8,8 @@ import java.util.Map;
 
 /*
 Task 1. Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся). Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем). Посчитать, сколько раз встречается каждое слово.
+
+Task 2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и телефонных номеров. В этот телефонный справочник с помощью метода add() можно добавлять записи, а с помощью метода get() искать номер телефона по фамилии. Следует учесть, что под одной фамилией может быть несколько телефонов (в случае однофамильцев), тогда при запросе такой фамилии должны выводиться все телефоны.
  */
 
 public class Main {
@@ -22,6 +24,22 @@ public class Main {
 
         Map<String, Integer> wordCounts = wordStats.getWordCounts();
         printWordCounts(wordCounts);
+
+        // Task 2
+        TelephoneDirectory directory = new TelephoneDirectory();
+
+        directory.add("Ivan", "123-456-7890");
+        directory.add("Egor", "987-654-3210");
+        directory.add("Jones", "555-555-5555");
+
+
+        System.out.println("Mark's numbers:");
+        for (String number : directory.get("Mark")) {
+            System.out.println(number);
+        }
+
+        System.out.println("\nJones's number:");
+        System.out.println(directory.get("Jones"));
 
     }
 
